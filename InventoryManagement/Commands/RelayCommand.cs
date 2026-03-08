91231d7eae97fcebfb831e00e9fdb8821662054e
Thien phuc <thienphuc484@gmail.com>
2026-03-08 10:49:59 +0700

    Add ReplayCommand

---
+++ InventoryManagement/Commands/RelayCommand.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Input;
+
+namespace InventoryManagement.Commands;
+
+public class RelayCommand(Action<object?> execute) : ICommand
+{
+    public bool CanExecute(object? parameter) => true;
+
+    public void Execute(object? parameter)
+    {
+        execute(parameter);
+    }
+
+    public event EventHandler? CanExecuteChanged;
+}
