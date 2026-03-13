57212f6f9ad24e5ae58b2a11da485646b185405b
Thien phuc <thienphuc484@gmail.com>
2026-03-13 10:07:05 +0700

    Add inventory and product model class

---
+++ InventoryManagement/Models/Inventory.cs
@@ -0,0 +1,9 @@
+﻿namespace InventoryManagement.Models;
+
+public class Inventory
+{
+    public int Id { get; set; }
+    public required Product Product { get; set; }
+    public int Stock { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
