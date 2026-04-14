66cebf83d1237aac99060702602dbe4664bbd3a4
Thien phuc <thienphuc484@gmail.com>
2026-04-14 10:14:18 +0700

    Add Roles to user model

--- InventoryManagement/Models/User.cs
+++ InventoryManagement/Models/User.cs
@@ -6,6 +6,7 @@ public class User
     public string? Username { get; set; }
     public string? Email { get; set; }
     public string? Password { get; set; }
+    public List<string> Roles { get; set; }
 
     public override string ToString()
     {
